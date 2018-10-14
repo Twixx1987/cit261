d36f764060c8da9999844272aaaf32d4f9d8ea5b
Shaun Densley <twixx1987@gmail.com>
2018-10-13 21:09:50 -0700

    Added JSON page

---
+++ web/Examples/JSON/json.js
@@ -0,0 +1,25 @@
+/*********************************************************************
+* This function is derived from code on W3Schools.com
+* Source: https://www.w3schools.com/js/tryit.asp?filename=tryjson_ajax
+**********************************************************************/
+// open the JSON RDI File
+function openJson(filename) {
+    // initiate a new XMLHttp request
+    var xmlhttp = new XMLHttpRequest();
+
+    // function to perform upon request completion
+    xmlhttp.onreadystatechange = function () {
+        // check for completion and ok status
+        if (this.readyState == 4 && this.status == 200) {
+            // create an object and parse the JSON file
+            var rdiObj = JSON.parse(this.responseText);
+
+            // loop through the object getting all the properties to display
+
+
+            // display the data from the JSON file in a nice format
+            document.getElementById("rdiDisplay").innerHTML = rdiObj;
+
+        }
+    }
+}

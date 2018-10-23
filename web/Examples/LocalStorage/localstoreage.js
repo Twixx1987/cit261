2dbe8870db5d2311e64d153d4de173a32ad83da9
Shaun Densley <twixx1987@gmail.com>
2018-10-23 15:10:12 -0700

    added local Storage pages

--- web/Examples/LocalStorage/localstoreage.js
+++ web/Examples/LocalStorage/localstoreage.js
@@ -22,44 +22,55 @@ function openJson(url, callbackFunc) {
     xmlhttp.send();
 }
 
-// generate the select options
-function loadOptions(xhttp) {
-    // create an object and parse the JSON file
-    var versionsObj = JSON.parse(xhttp.responseText);
+/*********************************************************************
+* This function is derived from code on W3Schools.com
+* Source: https://www.w3schools.com/jsref/prop_win_localstorage.asp
+**********************************************************************/
+// load the json file contents into local storage
+function loadStorage(xhttp) {
+    // check for storage support
+    if (typeof (Storage) !== "undefined") {
+        // create an object and parse the JSON file
+        var pandemicObj = JSON.parse(xhttp.responseText);
 
+        // loop through the object adding properties to local storage
+        for (var version in pandemicObj.versions) {
+            // append version to local storage
+            localStorage.setItem(pandemicObj.versions[version], pandemicObj.versions[roles]);
+        }
+
+        // load the select options
+        loadOptions();
+    } else {
+        // create an error string to display
+        var error = "Your browser does not support web storage,"
+                  + " this page requires local storage support.";
+
+        // Notify the user that local storage is not supported
+        document.getElementById("pandemicDisplay").innerHTML = error;
+    }
+}
+
+// generate the select options
+function loadOptions() {
     // get the select element
     var selectlist = document.getElementById("version");
 
-    // loop through the object members adding items 
-    for (var version in versionsObj.versions) {
+    // get the length of the localStorage object
+    var lslen = localStorage.length;
+
+    // loop through the local storage keys adding them to the select list
+    for (var count = 0; count < lslen; count++) {
         // create the option element
         var option = document.createElement("option");
 
         // set its properties
-        option.setAttribute("value", versionsObj.versions[version].version);
-        option.innerHTML = versionsObj.versions[version].version;
+        option.setAttribute("value", localStorage.key(count));
+        option.innerHTML = localStorage.key(count);
 
         // add the option to the list
         selectlist.appendChild(option);
     }
-}
-
-// display the RDI File contents
-function openRDIfile(xhttp) {
-    // create an object and parse the JSON file
-    var rdiObj = JSON.parse(xhttp.responseText);
-
-    // Create a Header
-    var txt = "<h1>" + rdiObj.game + "</h1>";
-
-    // loop through the object getting all the properties to display
-    for (var version in rdiObj.versions) {
-        // append the formatted data
-        txt += displayVersion(rdiObj.versions[version]);
-    }
-
-    // display the data from the JSON file in a nice format
-    document.getElementById("rdiDisplay").innerHTML = txt;
 }
 
 // display the selected version

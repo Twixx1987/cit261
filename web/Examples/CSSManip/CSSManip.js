9224117980f1d34e1c8ff86b2032661b08b273c3
Shaun Densley <twixx1987@gmail.com>
2018-11-15 23:21:48 -0800

    begin making the content and CSS changes

--- web/Examples/CSSManip/CSSManip.js
+++ web/Examples/CSSManip/CSSManip.js
@@ -1,1 +1,19 @@
 // JavaScript source code
+function startStory() {
+    // create session storage and set the count to 1
+    sessionStorage.clickCount = 1;
+
+    // change the content of the start button
+    let start = document.getElementById("start");
+    start.innerHTML = "Transform?";
+    start.style.color = "#0033cc";
+    start.style.backgroundColor = "#ffff66";
+
+    // change the display of demo outer
+    let demoOuter = document.getElementById("demoOuter");
+    demoOuter.style.display = block;
+}
+
+function part1() {
+
+}

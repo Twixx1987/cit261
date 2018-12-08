bf7a4510dca2c508182d8af7d58024cdb6389ffd
Shaun Densley <twixx1987@gmail.com>
2018-12-08 12:27:29 -0800

    Next phase of creation

---
+++ web/Project/RandomizerProject.js
@@ -0,0 +1,8 @@
+function menuToggle() {
+    var x = document.getElementById("menu");
+    if (x.className === "menu") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu";
+    }
+}
